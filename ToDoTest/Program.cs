6558fc075a22affb701c87741ed24356752e4460
Polina Moore <polina.moore@argusmedia.com>
2022-03-28 12:45:38 +0100

    Add  basic code

--- ToDoTest/Program.cs
+++ ToDoTest/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 namespace ToDoTest
 {
@@ -11,54 +15,22 @@ namespace ToDoTest
         private static ServiceProvider _serviceProvider;
         public static void Main(string[] args)
         {
-            CreateDI();
-            var worker = _serviceProvider.GetService<Worker>();
-            worker.Test();
+            //CreateDI();
+            //var worker = _serviceProvider.GetService<Worker>();
+            //worker.Test();
 
 
-
-            var todo = new ToDo();
-            todo.Title = "Task 1";
-            todo.Description = "My first task";
-            todo.CreatedDate = DateTime.Now;
-            _container.Add(todo);
-
-            IEnumerable<ToDo> GetAllTasks()
-            {
-                var list = new List<ToDo>();
-                foreach (var item in _container)
-                {
-                    if (item.IsCompleted)
-                        list.Add(item);
-                }
-                return list;
-            }
-        }
-
-
-        void SaveToFile()
-        {
-            //save to file
-        }
-
-        void ReadFromFile()
-        {
-            // read from file
-        }
-
-        void Login()
-        {
 
         }
 
 
-        public static void CreateDI()
-        {
-            var service = new ServiceCollection();
-            service.AddSingleton(typeof(UserKeeper));
-            service.AddScoped(typeof(Worker));
+        //public static void CreateDI()
+        //{
+        //    var service = new ServiceCollection();
+        //    service.AddSingleton(typeof(UserKeeper));
+        //    service.AddScoped(typeof(Worker));
 
-            _serviceProvider = service.BuildServiceProvider();
+        //    _serviceProvider = service.BuildServiceProvider();
         }
     }
 
@@ -69,41 +41,71 @@ namespace ToDoTest
         private readonly DateTime _creationDate = DateTime.Today;
         public DateTime? CompletionDate { get; set; }
         public bool IsCompleted { get; set; }
-        public string Owner { get; private set; }
+        public string User { get; private set; }
+        private ToDoController _todoController { get; set;}
 
-        public ToDo(string title, string description, string owner)
+        public ToDo(string title, string description, string user)
         {
             Title = title;
             Description = description;
-            Owner = owner;
+            User = user;
         }
     }
 
     public class ToDoController
     {
-        private static List<ToDo> _container = new List<ToDo>();
+        public static List<ToDo> AllTasks = new List<ToDo>();
 
-        public static void AddTask(string title, string description, string owner)
-        {
-            _container.Add(new ToDo(title, description, owner));
-        }
+        public static void AddTask(string title, string description, string user) => AllTasks.Add(new ToDo(title, description, user));
 
 
-        public static IEnumerable<ToDo> GetCompletedTasks()
-        {
-            var list = new List<ToDo>();
-            foreach (var item in _container)
-            {
-                if (item.IsCompleted)
-                    list.Add(item);
-            }
-            return list;
-        }  // IEnumerable is read-only, it's just for iterating through a collection, for querying data from in-memory collections like List, Array etc
+        public static List<ToDo> GetAllTasksForUser(string user) => AllTasks.FindAll(task => task.User == user);
+
+
+        public static void CompleteTask(string title, string user) => GetAllTasksForUser(user).FirstOrDefault(task => task.Title == title).IsCompleted = true;
+
+
+        public static IEnumerable<ToDo> GetCompletedTasks(string user) => GetAllTasksForUser(user).Where(toDo => toDo.IsCompleted == true).ToList();
+
+
+        public static void DeleteTask(string title, string user) => GetAllTasksForUser(user).RemoveAll(task => task.Title == title);
     }
+
 
     public class User
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        private string Username { get; set; }
+        private string _password { get; set; }
+        private UserController _userController { get; set; }
+
+        public User(string username, string password, UserController userController)
+        {
+            Username = username;
+            _password = password;
+            _userController = userController;
+        }
+    }
+
+    public class UserController
+    {
+        public bool IsLoggedIn { get; set; }
+
+        public void Login() => IsLoggedIn = true;
+        public void Logout() => IsLoggedIn = false;
+    }
+
+    // to be injected into both controllers?
+    public class FileUtility
+    {
+        void SaveToFile(object record, string filepath)
+        {
+            string jsonString = JsonSerializer.Serialize(record);
+            File.WriteAllText(filepath, jsonString);
+        }
+
+        void ReadFromFile(object record, string filepath)
+        {
+            File.ReadAllText(filepath);
+        }
     }
 }
